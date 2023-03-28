bfa587f4aa558fb53c475c1fa0afa54dee2f2850
programmerShinobi <faqihpratamamuhti@gmail.com>
2023-03-28 13:32:01 +0700

    update postgreeSQL - day-1

--- day1/01-regions.sql
+++ day1/01-regions.sql
@@ -1,36 +1,81 @@
 -- Create table regions
 CREATE TABLE regions (
-	region_id serial,
-	region_name varchar(25),
-	CONSTRAINT region_id_pk PRIMARY KEY (region_id)
+	region_id SERIAL,
+	region_name VARCHAR(25),
+	CONSTRAINT region_id_pk 
+		PRIMARY KEY (region_id)
 );
+
+-- insert values table regioons
+INSERT INTO regions(region_name)
+	VALUES
+	('asia'),
+	('america'), 
+	('australia');
+
+-- update region_name
+UPDATE regions 
+	SET region_name = 'eropa' 
+		WHERE region_id = 2;
 
 -- view all field table regions
 SELECT * FROM regions;
 
+-- create table countries
+CREATE TABLE countries (
+	country_id SERIAL,
+	country_name VARCHAR(25),
+	region_id INT,
+	CONSTRAINT country_id_pk 
+		PRIMARY KEY (country_id)
+);
+
+-- add constraint fk_region_id in table countries (region_id)
+ALTER TABLE countries 
+	ADD CONSTRAINT fk_region_id 
+		FOREIGN KEY (region_id)
+			REFERENCES regions(region_id);
+
+--insert values in countries
+INSERT INTO countries(
+	country_name,
+	region_id
+) VALUES (
+	'indonesia',
+	1
+),(
+	'germany',
+	2
+),(
+	'queensland',
+	3
+);
+
+-- view join table regions & countries
+SELECT * 
+	FROM regions re
+	LEFT JOIN countries co 
+		ON co.region_id = re.region_id
+		ORDER BY re.region_id 
+			DESC;
+
 -- add field table regions
-ALTER TABLE regions ADD COLUMN region_x VARCHAR(30);
-
--- add constraint region_x_pk
-ALTER TABLE regions ADD CONSTRAINT region_x_pk PRIMARY KEY (region_x);
-
--- add constraint region_x_fk
-ALTER TABLE regions ADD CONSTRAINT region_x_fk FOREIGN KEY table2(region_x);
+ALTER TABLE regions 
+	ADD COLUMN region_x VARCHAR(30);
 
 -- edit field region_x in table regions
-ALTER TABLE regions RENAME COLUMN region_x to region_y;
+ALTER TABLE regions 
+	RENAME COLUMN region_x
+		TO region_y;
 
 -- edit lenght field region_y to VARCHAR(60)
-ALTER TABLE regions ALTER COLUMN region_y TYPE VARCHAR(60);
+ALTER TABLE regions 
+	ALTER COLUMN region_y 
+		TYPE VARCHAR(60);
 
 -- delete field region_y in table regions
-ALTER TABLE regions DROP COLUMN region_y;
-
--- delete table regions
-DROP TABLE regions;
-
--- drop field region_x
-ALTER TABLE regions DROP COLUMN region_x;
+ALTER TABLE regions 
+	DROP COLUMN region_y;
 
 -- drop table regions
 DROP TABLE regions;
@@ -38,8 +83,3 @@ DROP TABLE regions;
 -- drop table region CASCADE
 DROP TABLE regions CASCADE;
 
--- insert values table regioons
-INSERT INTO regions(region_name) VALUES('malaysia');
-
--- update region_name
-UPDATE regions SET region_name = 'Malaysia' WHERE region_id = 2;
